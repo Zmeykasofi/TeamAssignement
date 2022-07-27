0c87b89a4932258e106b8e87074d5e372c16883e
Софья Полянкина <zmeykasofi@mail.ru>
2022-07-27 15:47:19 +0700

    Minus features added

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -6,10 +5,12 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        String[] products = {"Молоко", "Хлеб", "Гречневая крупа"};
-        int[] prices = {50, 14, 80}; // для сумм с копейками можно ввести double
+        String[] products = {"Молоко", "Хлеб", "Гречневая крупа", "Сахар", "Соль"};
+        int[] prices = {50, 14, 80, 75, 33}; // для сумм с копейками можно ввести double
         int[] basket = new int[products.length];
-        int sumProducts = 0;
+        int basketSum = 0;
+        int productNumber = 0;
+        int productCount = 0;
 
         while (true) {
             System.out.println("Список возможных товаров для покупки:");
@@ -25,8 +26,6 @@ public class Main {
                 break;
             }
             String[] purchases = input.split(" ");
-            int productNumber;
-            int productCount;
             if (purchases.length != 2) {
                 System.out.println("Некорректный ввод!!! - введите: номер товара от 1 до 3 и количество товара");
                 continue;
@@ -40,26 +39,31 @@ public class Main {
                 if (productNumber < 0 || productNumber > products.length) {
                     System.out.println("Ошибка: Такого товара нет");
                     continue;
-                } else if (productCount < 0) {
-                    System.out.println("Ошибка: Введено некорректное количество товара");
-                    continue;
                 }
-                sumProducts += prices[productNumber] * productCount;
-                basket[productNumber] += productCount;
             } catch (RuntimeException e) {
                 System.out.println("Введена некорректная информация");
             }
+
+            if (productCount == 0) {
+                basket[productNumber] = 0;
+            } else {
+                if (basket[productNumber] + productCount < 0) {
+                    System.out.println("Количество товаров в корзине не может быть отрицательным!");
+                    continue;
+                }
+                basket[productNumber] += productCount;
+            }
         }
+
         System.out.println("Ваша корзина:");
         for (int i = 0; i < basket.length; i++) {
             if (basket[i] != 0) {
                 System.out.println(products[i] + " " + basket[i] + " шт " + prices[i] + " руб/шт " + (basket[i] * prices[i]) + " рублей в сумме");
-
+                basketSum += prices[i] * basket[i];
             }
         }
 
-        System.out.println("Итого: " + sumProducts);
-
+        System.out.println("Итого к оплате: " + basketSum + " руб.");
     }
 }
 
